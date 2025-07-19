2dcf497855a8b169f839251ae3a210745a80e2ea
altinaD <altynberdiyeva99@gmail.com>
2025-07-19 16:29:36 +0500

    new location to map

--- app/[locale]/contact/map.tsx
+++ app/[locale]/contact/map.tsx
@@ -3,13 +3,21 @@ const GoogleMap = () => {
         <div style={{
             width: '100%', height: '100%'
         }}>
-            <iframe
+            {/* <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d786.8355433658827!2d58.37553814644386!3d37.922436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f6ffd096273235d%3A0x20c23ef83b2dcda3!2sCaspy%20Creative!5e0!3m2!1sen!2s!4v1740559444574!5m2!1sen!2s"
                 width="100%"
                 height="100%"
                 style={{borderRadius: '17px' }}
                 allowFullScreen
                 loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade">
+            </iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3145.2219505706626!2d58.401652276425786!3d37.9719493007767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sg.%20kuly%C3%BDew%20(2127)%20begenchli!5e0!3m2!1sru!2s!4v1752924447268!5m2!1sru!2s"
+                width="100%" 
+                height="100%" 
+                style={{borderRadius: '17px' }}
+                allowFullScreen
+                loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade">
             </iframe>
         </div>
